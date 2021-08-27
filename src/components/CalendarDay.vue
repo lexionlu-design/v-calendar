b24844b5aa8aeead988abf091cab83dba3912310
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2021-08-27 11:38:23 +0800

    Build Calendar date picker

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -188,17 +188,17 @@ export default {
       } else if (this.day.inMonth) {
         tabindex = '-1';
       }
-      let props = {
+      const props = {
         tabindex,
         'aria-label': this.day.ariaLabel,
         'aria-disabled': this.day.isDisabled ? 'true' : 'false',
         role: 'button',
       };
-      const isAriaCurrent = !!this.day.attributes.find(({key})=>key==='today');
+      const isAriaCurrent = !!this.day.attributes.find(({ key }) => key === 'today');
       if (isAriaCurrent) {
         props['aria-current'] = 'date';
       }
-      const isAriaSelected = !!this.day.attributes.find(({key})=>key==='select-drag');
+      const isAriaSelected = !!this.day.attributes.find(({ key }) => key === 'select-drag');
       if (isAriaSelected) {
         props['aria-selected'] = 'true';
       } else {
